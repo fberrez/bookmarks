5391253533b605b692fe6cde7a545839a722b2b7
fberrez <bf0c6670-0084@protonmail.com>
2021-12-29 20:01:20 +0100

    feat(bookmarks): impl. `GET /bookmarks/:id`

--- containers/mongo/docker-entrypoint-initdb.d/init.js
+++ containers/mongo/docker-entrypoint-initdb.d/init.js
@@ -4,11 +4,13 @@ const conn = new Mongo('mongodb://root:example@localhost:27017');
 const bookmarksDB = conn.getDB('bookmarks'); 
 bookmarksDB.getCollection('bookmarks').insertMany([
   {
+    _id: ObjectId("61ccaf886cc94de7adf32faf"),
     type: 'vimeo',
     url: 'https://vimeo.com/659409353',
     keywords: ['life', 'ai', 'insurance', 'algorithm']
   },
   {
+    _id: ObjectId("61ccaf911c1db63ccd104b6f"),
     type: 'flickr',
     url: 'https://flic.kr/p/2jz6N2S',
     keywords: ['running', 'late', 'train']
