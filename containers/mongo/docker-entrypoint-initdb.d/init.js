4807fdbc7fdb0e84dd9867b202219c1069f8ac21
fberrez <bf0c6670-0084@protonmail.com>
2021-12-30 14:50:36 +0100

    feat(bookmarks): impl. `GET /bookmarks`

--- containers/mongo/docker-entrypoint-initdb.d/init.js
+++ containers/mongo/docker-entrypoint-initdb.d/init.js
@@ -14,7 +14,70 @@ bookmarksDB.getCollection('bookmarks').insertMany([
     type: 'flickr',
     url: 'https://flic.kr/p/2jz6N2S',
     keywords: ['running', 'late', 'train']
-  }
+  },
+  {
+    type: 'flickr',
+    url: 'https://flic.kr/p/bs7NH4',
+    keywords: ['mountain', 'sun', 'water']
+  },
+  {
+    type: 'flickr',
+    url: 'https://flic.kr/p/quRVQM',
+    keywords: ['mountain', 'sun', 'snow', 'house']
+  },
+  {
+    type: 'flickr',
+    url: 'https://flic.kr/p/2mhvEjU',
+  },
+  {
+    type: 'flickr',
+    url: 'https://flic.kr/p/2mGaRZ6',
+    keywords: ['lake', 'house', 'mountain', 'morning']
+  },
+  {
+    type: 'flickr',
+    url: 'https://flic.kr/p/2mQ9Yvs',
+    keywords: ['christmas', 'coffee']
+  },
+  {
+    type: 'vimeo',
+    url: 'https://vimeo.com/653826310',
+  },
+  {
+    type: 'vimeo',
+    url: 'https://vimeo.com/652039454',
+    keywords: ['facility']
+  },
+  {
+    type: 'vimeo',
+    url: 'https://vimeo.com/660521773',
+    keywords: ['straight', 'robot']
+  },
+  {
+    type: 'vimeo',
+    url: 'https://vimeo.com/646793833',
+    keywords: ['paving', 'toundra']
+  },
+  {
+    type: 'vimeo',
+    url: 'https://vimeo.com/659409353',
+    keywords: ['forever']
+  },
+  {
+    type: 'vimeo',
+    url: 'https://vimeo.com/658911809',
+    keywords: ['happinness','journey']
+  },
+  {
+    type: 'flickr',
+    url: 'https://flic.kr/p/2kGkvAv',
+    keywords: ['fox']
+  },
+  {
+    type: 'flickr',
+    url: 'https://flic.kr/p/2kMtzJj',
+    keywords: ['desert','mountain']
+  },
 ]);
 
 // create new user
